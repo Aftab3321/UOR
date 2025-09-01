926cd539eefd7729bf7f7fe41eb7ad59299f2737
Aftab Ahmed <aftabjamali1212@gmail.com>
2025-09-01 23:56:44 +0500

    minified files added

---
+++ assets/javascript/main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const lazyImages = document.querySelectorAll("img.lazy");
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const img = entry.target;
+        img.src = img.dataset.src; // swap placeholder with real image
+        img.classList.remove("lazy");
+        observer.unobserve(img);
+      }
+    });
+  });
+
+  lazyImages.forEach(img => observer.observe(img));
+});
